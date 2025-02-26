9d0f7695a9f2d1e548b59b296d209f0c42e4eba8
braincell044 <apple@APPLEs-MacBook-Pro.local>
2025-02-26 14:57:15 +0100

    store update

--- src/app/store/page.js
+++ src/app/store/page.js
@@ -1,222 +1,100 @@
-import React from 'react'
-import "@/styles/App.css"; // ✅ Use an alias
-import Coursel from '@/app/Coursel';
-import BigHead from '@/components/BigHead';
-import Footer from '@/components/Footer';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "@/styles/page.module.css";
+import BigHead from "@/components/BigHead";
+import Footer from "@/components/Footer";
+import "@/styles/App.css"; 
+import Coursel from "../Coursel";
 import SingleProduct from "../../components/SingleProduct";
 
+const products = [
+  { id: 1, name: " MacBook Air 13-inch with M1 chip", price: "$1050", image: "https://res.cloudinary.com/drbiup2zg/image/upload/v1740561707/Rectangle_36_zc6dvx.png" },
+  { id: 2, name: "MacBook Air 13-inch with M2 chip", price: "$2000", image: "https://res.cloudinary.com/drbiup2zg/image/upload/v1740562218/Rectangle_38_juhopr.png" },
+  { id: 3, name: " MacBook Pro 13-inch", price: "$3250", image: "https://res.cloudinary.com/drbiup2zg/image/upload/v1740562218/Rectangle_37_qbqa8q.png" },
+];
+const productTwo = [
+  { id: 1, name: " iPhone 14 Pro Max", price: "$1900", image: "https://res.cloudinary.com/drbiup2zg/image/upload/v1740577089/Rectangle_39_xkn6qq.png" },
+  { id: 2, name: "iPhone 14 ", price: "$800", image: "https://res.cloudinary.com/drbiup2zg/image/upload/v1740577099/Rectangle_41_qyw56l.png" },
+  { id: 3, name: "iPhone 14 Pro", price: "$700", image: "https://res.cloudinary.com/drbiup2zg/image/upload/v1740577089/Rectangle_39_xkn6qq.png" },
+];
 
-
-
-
+const productThree = [
+  { id: 1, name: " iPad Pro 12.9 Inch", price: "$1900", image: "https://res.cloudinary.com/drbiup2zg/image/upload/v1740562644/Rectangle_42_tkahzj.png" },
+  { id: 2, name: "Pad 10th Gen ", price: "$800", image: "https://res.cloudinary.com/drbiup2zg/image/upload/v1740562644/Rectangle_44_gkzfwd.png" },
+  { id: 3, name: "iPad Pro 11 Inch",price: "$700", image: "https://res.cloudinary.com/drbiup2zg/image/upload/v1740562644/Rectangle_43_bswnax.png" },
+];
 
 const Store = () => {
   return (
-    <div className='overflow-h'>
-      <BigHead/>
-        <div className='px-4'>
-        <h2>Store</h2>
-        <div className='bar'></div>
-        </div>
-
-       
-<Coursel/>
-
-<div className='px-4'>
-    <h4>All models Mac</h4>
-    <div className='bar-2'></div>
-</div>
-
-<div className="container text-center py-5 mx-5">
-  <div className="row">
-    <div className="col">
-        <div className='mac card-1'>
-        <p>Buy MacBook Air 13-inch with M1 chip</p>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-         <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button className='btn mybtn '>Buy now</button>
-
-        </div>
-
+   <div>
+    <BigHead/>
+    <div className="mx-5">
+      <h3>Store</h3>
+      <div className="bar"></div>
+      <Coursel/>
     </div>
-    <div className="col">
 
- <div className='mactwo card-1'>
- <p>Buy MacBook Air 13-inch with M2 chip</p>
- <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-         <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button className='btn mybtn '>Buy now</button>
- </div>
+<div className={`container ${styles.storeContainer}`}>
+<div className="mx-5 my-5">
+      <h3>All Model Mac</h3>
+      <div className="bar-3"></div>
+    
     </div>
-    <div className="col">
-    <div className='macthree card-1'>
- <p>Buy MacBook pro 13-inch</p>
- <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-         <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button className='btn mybtn '>Buy now</button>
- </div>
+      <div className="row">
+        {products.map((product) => (
+          <div key={product.id} className="col-md-4 mb-4">
+            <div className={`card ${styles.productCard}`}> 
+              <img src={product.image} className={`card-img-top ${styles.productImage}`} alt={product.name} />
+              <div className="card-body text-center">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">{product.price}</p>
+                <button className="btn btn-primary">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="mx-5 my-5">
+      <h3>All Model Iphone</h3>
+      <div className="bar-2"></div>
+    
     </div>
-  </div>
-</div>
-<div className='px-4 py-4'>
-    <h4>All models iPhone</h4>
-    <div className='bar-3'></div>
-</div>
 
-<div className="container text-center mx-4">
-  <div className="row">
-    <div className="col">
-  <div className='iphone card-1'>
-<p>Buy iPhone 14 Pro </p>
-<br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-         <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button className='btn mybtn '>Buy now</button>
+      <div className="row">
+        {productTwo.map((product) => (
+          <div key={product.id} className="col-md-4 mb-4">
+            <div className={`card ${styles.productCard}`}> 
+              <img src={product.image} className={`card-img-top ${styles.productImage}`} alt={product.name} />
+              <div className="card-body text-center">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">{product.price}</p>
+                <button className="btn btn-primary">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
 
-  </div>
+
+      <div className="row">
+        {productThree.map((product) => (
+          <div key={product.id} className="col-md-4 mb-4">
+            <div className={`card ${styles.productCard}`}> 
+              <img src={product.image} className={`card-img-top ${styles.productImage}`} alt={product.name} />
+              <div className="card-body text-center">
+                <h5 className="card-title">{product.name}</h5>
+                <p className="card-text">{product.price}</p>
+                <button className="btn btn-primary">Add to Cart</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-    <div className="col">
-    <div className='iphonetwo card-1'>
-    <p>Buy iPhone 14 Plus</p>
-    <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-         <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button className='btn mybtn '>Buy now</button>
-</div>
-    </div>
-    <div className="col">
-    <div className='iphonethree  card-1 '>
-    <p>Buy iPhone 14 Pro max</p>
-    <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-         <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button className='btn mybtn '>Buy now</button>
-
-</div>
-    </div>
-  </div>
-</div>
-
-
-<div className='px-4 py-4'>
-    <h4>All models iPad</h4>
-    <div className='bar-2'></div>
-</div>
-<div className="container text-center py-5 mx-5">
-  <div className="row">
-    <div className="col">
-   <div className='ipad card-1'>
-
-<p>Buy iPad Pro 12.9 Inch</p>
-<br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-         <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button className='btn mybtn '>Buy now</button>
-
-
+    <SingleProduct/>
+    <Footer/>
    </div>
-    </div>
-    <div className="col">
-<div className='ipadtwo card-1'>
-<p>Buy iPad Pro 11 Inch</p>
-<br/>
-<p>iPad Pro 11 Inch</p>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-         <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button className='btn mybtn '>Buy now</button>
-</div>
-    </div>
-    <div className="col">
-<div className='ipadthree card-1'>
-<p> Buy iPad 10th Gen</p>
-<br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-         <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <button className='btn mybtn '>Buy now</button>
+  );
+};
 
-</div>
-    </div>
-  </div>
-</div>
-<SingleProduct/>
-<Footer/>
-    </div>
-  )
-}
-
-export default Store
+export default Store;
