a75dc56ae7cae22381405db712436965cf890d17
braincell044 <apple@APPLEs-MacBook-Pro.local>
2025-04-17 02:59:44 +0100

    update

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,31 +3,41 @@ import { Carousel, Container } from "react-bootstrap";
 
 const CardImages = [
   {
-    url: "https://res.cloudinary.com/dvfiw24p4/image/upload/v1744843703/Screenshot_2025-04-16_at_23.44.01_nf4vkr.png",
+    url: "https://res.cloudinary.com/dvfiw24p4/image/upload/v1744843703/Screenshot_2025-04-16_at_23.44.01_nf4vkr.png?v=2",
     alt: "Card 1",
   },
   {
-    url: "https://res.cloudinary.com/dvfiw24p4/image/upload/v1744819957/Screenshot_2025-04-16_at_16.40.36_egzic8.png",
+    url: "https://res.cloudinary.com/dvfiw24p4/image/upload/v1744819957/Screenshot_2025-04-16_at_16.40.36_egzic8.png?v=2",
     alt: "Card 2",
   },
 ];
 
 const CardCarousel = () => {
   return (
-    <Container fluid className="my-4 px-md-3 px-2">
+    <Container fluid className="my-4 p-0">
       <Carousel indicators={false} controls={true} interval={3000} fade>
         {CardImages.map((card, index) => (
-          <Carousel.Item key={index} style={{ height: "20vh", minHeight: "300px" }}>
-            <div className="d-flex justify-content-center h-100">
+          <Carousel.Item 
+            key={index} 
+            className="d-block w-100"
+            style={{ 
+              height: "50vh",
+              minHeight: "250px",
+              maxHeight: "600px"
+            }}
+          >
+            <div className="d-flex justify-content-center align-items-center h-100">
               <img
                 src={card.url}
                 alt={card.alt}
                 className="img-fluid shadow-lg"
                 style={{
-                  width: "100%",
-                  height: "100%",
-                  borderRadius: "30px",
-                  objectFit: "contain",
+                  width: "auto",
+                  maxWidth: "100%",
+                  height: "auto",
+                  maxHeight: "100%",
+                  borderRadius: "20px",
+                  objectFit: "scale-down",
                   objectPosition: "center",
                 }}
               />
